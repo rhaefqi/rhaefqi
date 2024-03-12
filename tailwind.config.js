ed8ba4eb809c66b84e334abfda8e8813cf46bc80
Rifqi <rifqijabrah@gmail.com>
2024-03-12 16:36:04 +0700

    tes tugas 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}', 'index.html'],
+  content: ['./public/**/*.{html,js}', './*.{html,js}'],
   darkMode: 'class',
   theme: {
     container: {
